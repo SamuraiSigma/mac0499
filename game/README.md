9c3dd10b4b6472e51b2a00db84f9229e2d3f941d
Leonardo Pereira Macedo <leonardo.macedo@usp.br>
2017-08-31 13:08:39 -0300

    Add script to easily run custom games

---
+++ game/README.md
@@ -0,0 +1,10 @@
+# *Speech Recognizer* Demos
+
+This directory contains simple games used to test/demonstrate the new *Speech
+Recognizer* module for *Godot*.
+
+## Execution
+
+To run a game, use the **run_game.sh** script with its directory as an argument:
+
+    ./run_game.sh game_directory/
---
+++ game/run_game.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Runs a Godot game directory by using the custom Godot build that has the Speech
+# Recognizer module.
+
+GODOTDIR=../godot
+GODOTBIN=godot*
+
+#----------------------------------------------------------------------
+# Shows how to use the script.
+
+function usage {
+    echo -e "\e[1mUSAGE\e[0m"
+    echo -e "\t./`basename $0` <game_directory>\n"
+
+    echo -e "\e[1mDESCRIPTION\e[0m"
+    echo -e "\tRuns the specified game with a custom Godot version that has the Speech Recognizer module.\n"
+
+    echo -e "\e[1mCOMMAND LINE ARGUMENTS\e[0m"
+    echo -e "\t\e[1m<game_directory>\e[0m\tGame directory containing an engine.cfg file."
+}
+
+# ---------------------------------------------------------------------
+
+if (($# < 1)); then
+    usage
+    exit 1
+fi
+
+gamedir=$1
+export LD_LIBRARY_PATH=`pwd`/$GODOTDIR/bin
+./$GODOTDIR/bin/$GODOTBIN -path $gamedir
